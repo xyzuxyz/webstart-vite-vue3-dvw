e9bd710b6d60cc8ff861aacfff5278b10994f9fa
kosku <kosgug@gmx.com>
2023-09-05 13:38:08 +0800

    update import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path"
-import postcssVwResize from "./src/libs/postcss-dvw-to-vw.js"
+import postcssVwResize from "@xyzu/postcss-dvw-to-vw"
 
 // https://vitejs.dev/config/
 export default defineConfig({
